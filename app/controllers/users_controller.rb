11a634ca20aa9cf72ccb36f13a860a2b302a4b5a
Brandon Cato <b.cato28@gmail.com>
2014-03-05 22:48:15 -0800

    add tunnlr gem and bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,16 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 		if @user.save
 			render text: "Awesome! It seems like my test app is working"
+
+			# Instantiate a Twilio client
+			client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+
+			# Create and send an SMS message
+			client.account.sms.messages.create(
+				from: TWILIO_CONFIG['from'],
+				to: @user.phone,
+				body: "If you're seeing this, it must mean you have fixed all (well maybe most) of the bugs in your code. Sweet."	
+				)
 		else
 			render :new
 		end
